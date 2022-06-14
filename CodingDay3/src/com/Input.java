46f769c72e17ab3989cd884c978aeb5965a00047
SanjayR857 <sanjayr9586@gmail.com>
2022-06-14 21:12:35 +0530

    FIRST COMMIT

---
+++ CodingDay3/src/com/Input.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.Scanner;
+
+//enter 3 variables and print them
+public class Input {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	int a=scan.nextInt();
+	int b=scan.nextInt();
+	int c=scan.nextInt();
+	System.out.println("output");
+	System.out.println(a+"\n"+b+"\n"+c);
+}
+}
+/*
+42
+46
+79
+output
+42
+46
+79
+*/
